cf4293577054f4bae78b535ec0fe3ca625a1ee0d
GoPhile <GoPhile@users.noreply.github.com>
2018-07-07 23:50:40 -0600

    Update IMmsProperty.java

--- IMmsProperty.java
+++ IMmsProperty.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package makemystay;
 import java.util.*;
 
 /**
  *
- * @author owner
+ * @author Josh Seaton
+ * Created: 6/28/2018
+ * Version: 1
  */
 public interface IMmsProperty {
+    public int getPropertyId();
     public IMmsUser getOwner();
     public List<IMmsUser> getDelegates();
     public String getPropertyName ();
