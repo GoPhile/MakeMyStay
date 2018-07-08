324535b6aa366ab1f6312bd4c2f3ace44880137f
GoPhile <GoPhile@users.noreply.github.com>
2018-07-07 23:56:18 -0600

    Update MmsDescriptorManager.java

--- MmsDescriptorManager.java
+++ MmsDescriptorManager.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package makemystay;
 
 /**
  *
- * @author owner
+ * @author Josh Seaton
+ * Created: 6/28/2018
+ * Version: 1
  */
 public class MmsDescriptorManager {
     
